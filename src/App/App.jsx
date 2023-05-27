8af878c37fb8a340a6dd431d6ec00ca3b46f954b
hgutierrez1 <hgutierrezva123@gmail.com>
2023-05-27 12:51:08 -0500

    Added folder structure

---
+++ src/App/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+      hola mundo
+    </div>
+        
+    </>
+  )
+}
+
+export default App
